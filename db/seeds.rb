f2089784886933bd56003d8af8719182b08ab521
Matthew Cwalina <matthewcwalina@gmail.com>
2021-11-11 14:41:01 +0100

    Using faker to create a database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'cleaning DB...'
+Movie.destroy_all
+
+puts 'creating some movies'
+5.times do
+  movie = Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg',
+    rating: rand(1..10)
+  )
+  puts "movie with id #{movie.id} created!"
+end
