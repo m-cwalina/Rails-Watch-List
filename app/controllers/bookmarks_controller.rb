0b3387432da86058728de303ee68252a726fcba2
Matthew Cwalina <matthewcwalina@gmail.com>
2021-11-12 13:55:33 +0100

    All tests are passing

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,13 @@
 class BookmarksController < ApplicationController
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(valid_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     if @bookmark.save
       redirect_to @list
     else
@@ -20,6 +23,6 @@ class BookmarksController < ApplicationController
   private
 
   def valid_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
