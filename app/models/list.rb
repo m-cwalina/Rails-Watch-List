f3deb4277ff002b5f74faa004abe19bab7daf63c
Matthew Cwalina <matthewcwalina@gmail.com>
2021-11-15 15:58:36 +0100

    Shit is awesome

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: tru
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
 end
